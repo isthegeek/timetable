ae14b2ab1b29063b1041babbef48759385b25b5b
Vijay Nandwani <born2bgeeky@gmail.com>
2015-12-26 14:31:11 +0530

    Fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,23 +10,6 @@
     </head>
 
     <body>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '935862053168239',
-      xfbml      : true,
-      version    : 'v2.5'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
 <nav>
     <div class="nav-wrapper">
       <a href="myffcs.in" class="brand-logo center">myFFCS</a>
