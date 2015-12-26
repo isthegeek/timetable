306f1e88f322bdac85792b6f11c3d9740c615bd8
Vijay Nandwani <born2bgeeky@gmail.com>
2015-12-27 01:44:23 +0530

    shamvil

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -65,6 +65,26 @@
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
+      <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-71776019-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- GDG VIT FFCS -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1192143571859738"
+     data-ad-slot="8088037006"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     </body>
   </html>
         
