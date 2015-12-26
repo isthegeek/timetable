9d402bb5c49625c409cb6f1f90e4048c9b4b28fa
Vijay Nandwani <born2bgeeky@gmail.com>
2015-12-26 14:36:55 +0530

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -117,6 +117,7 @@
         {!! Form::submit('Register', array('class'=>'waves-effect waves-light btn')) !!}
     {!! Form::close() !!}
 
+
 <a href="https://www.facebook.com/dialog/share?redirect_uri=http%3A%2F%2Ftimetable.cloudapp.net%2Fhome&app_id=481083698767151&display=popup&href={!! action('TimeTableController@share', ['id', $id]) !!}">Share on Facebook</a>
   <div id="target">
 <table class = "highlight">
