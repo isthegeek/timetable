5b13841c830d1039adb493d38afbfad12e6e619a
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-12-26 20:37:48 +0530

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,20 @@
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}" media="screen,projection"/>
       <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta charset="UTF-8">
+      <meta http-equiv="refresh" content="1 day">
+      <meta name="description" content="VIT - FFCS Timetable. Create your timetable and experience the FFCS process before FFCS. Share it ">
+      <meta name="keywords" content="VIT,FFCS, Timetable, VIT UNIVERSITY, GDG,GDG VIT, VELLORE, COURSE ALLOCATION">
+      <meta name="distribution" content="web">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+       <meta name="language" content="en-us">
+       <meta name="distribution" content="Global" />
+        <meta property="og:title" content="myFFCS- Create your timetable before FFCS">
+    <meta property="og:site_name" content="myFFCS ">
+    <meta property="og:url" content="https://www.myffcs.in">
+    <meta property="og:description" content="VIT - FFCS Timetable. Create your timetable and experience the FFCS process before FFCS. Share it">
+      <meta name="author" content="Ashwini Purohit, Vijay Nandwani">
     </head>
 
     <body>
