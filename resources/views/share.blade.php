873c2f5414813d194fe8b123d8616d690e8212e9
Vijay Nandwani <born2bgeeky@gmail.com>
2015-12-27 01:43:53 +0530

    shamvil

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -145,13 +145,13 @@
       <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
        <script type="text/javascript" src="{{ asset('js/html2canvas.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/jquery.plugin.html2canvas.js') }}"></script>
-        <script>
+<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-71770061-1', 'auto');
+  ga('create', 'UA-71776019-1', 'auto');
   ga('send', 'pageview');
 
 </script>
