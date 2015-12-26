62e34b1f3d117020d1714dc93e2578514e7ba60d
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-12-26 23:42:47 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -272,13 +272,13 @@
       <li><a class="btn-floating blue" onclick="share();"><i class="fa fa-facebook"></i></a></li>
     </ul>
   </div>
-           
+           <footer class = "page-footer">
           <div class="footer-copyright">
-            <div class="container">
+            
             Feel free to contribute. By : GDG VIT VELLORE
-           </div>
+          
             </div>
-         
+         </footer>
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
