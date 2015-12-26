e08009a46ff953c93c9ed9b141abc23709359aac
ashwini <ashwinipurohit@yahoo.in>
2015-12-27 03:33:10 +0530

    Analytic code change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -315,7 +315,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-71776019-1', 'auto');
+  ga('create', 'UA-71735575-1', 'auto');
   ga('send', 'pageview');
 
 </script>
