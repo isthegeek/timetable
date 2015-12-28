4472e23ff75390ba7a971b228eb7041344da4cc6
Vijay Nandwani <born2bgeeky@gmail.com>
2015-12-28 11:08:53 +0530

    Fix /day redirect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,11 @@
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta charset="UTF-8">
-      <meta http-equiv="refresh" content="1 day">
       <meta name="description" content="VIT - FFCS Timetable. Create your timetable and experience the FFCS process before FFCS. Share it ">
       <meta name="keywords" content="VIT,FFCS, Timetable, VIT UNIVERSITY, GDG,GDG VIT, VELLORE, COURSE ALLOCATION">
       <meta name="distribution" content="web">
       <meta name="apple-mobile-web-app-capable" content="yes">
        <meta name="language" content="en-us">
-       <meta name="distribution" content="Global" />
         <meta property="og:title" content="myFFCS- Create your timetable before FFCS">
     <meta property="og:site_name" content="myFFCS ">
     <meta property="og:url" content="https://www.myffcs.in">
